be292babe3557a233f7a99b3f3c66a04c117133e
minyez <minyez@iphy.ac.cn>
2022-02-11 19:50:05 +0800

    implemented own cif

--- mushroom/core/cif.py
+++ mushroom/core/cif.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """this module defines the object to process .cif files"""
+# pylint: disable=C0209,W1514
 import os
 import re
+from typing import List
+from io import StringIO
 try:
     import CifFile
 except ImportError:
     CifFile = None
 
 from mushroom.core.crystutils import get_latt_vecs_from_latt_consts, get_all_atoms_from_symops
-from mushroom.core.ioutils import raise_no_module
+from mushroom.core.ioutils import raise_no_module, open_textio
 from mushroom.core.data import conv_estimate_number, closest_frac
 from mushroom.core.logger import create_logger
 
@@ -28,35 +31,212 @@ class _CifFile:
     The item name must starts with an underscore.
 
     The loop starts with a single 'loop_', followed by names of containing items, each as a line.
-    The first line that does not start with an underscore is treated as the start of the data entries.
-    The data entries stops at a new line, loop or item.
+    The first line that does not start with an underscore is treated
+    as the start of the data entries. The data entries stops at a new line, loop or item.
 
     This object is intended to replace the external CifFile object.
     The main reason is to remove the PyCIFRW dependency.
-
-    The object accepts any number of data blocks.
-    Each block is a dict, containing three keys:
-        name: a string, charactering the data block
-        items: a dict, the simple key-value pair in CIF
-        loops: a list, each member is a dict containing "keys" (a list) and "values" (list of lists)
     """
     def __init__(self, *blks):
-        pass
+        self._blks = blks
 
-    def _export(self):
+    def get_blk(self, iblk):
+        """get the data block"""
+        return self._blks[iblk]
+
+    def first_block(self):
+        """return the first data block"""
+        return self._blks[0]
+
+    @property
+    def nblk(self):
+        """number of data blocks"""
+        return len(self._blks)
+
+    def export(self):
         """export the content as a string"""
-        slist = []
-        return '\n'.join(slist)
+        raise NotImplementedError
 
     def write(self, pcif):
         """write the CIF content to a file"""
         with open(pcif, 'w') as _h:
-            print(self._export(), file=_h)
+            print(self.export(), file=_h)
+
+    @classmethod
+    def build(cls, name: str, items: dict, loops: List[dict]):
+        """create a CIF object containing single data block
+
+        See CifBlk object for the arguments
+        """
+        blk = _CifBlk(name, items, loops)
+        return cls(blk)
 
     @classmethod
     def read(cls, pcif):
         """read the CIF file"""
-        raise NotImplementedError
+        with open_textio(pcif) as h:
+            lines = h.readlines()
+        # search data block
+        blks_st = [i for i, l in enumerate(lines) if l.startswith("data_")]
+        blks = [_CifBlk.read(StringIO(''.join(lines[st:blks_st[i+1]])))
+                for i, st in enumerate(blks_st[:-1])]
+        blks.append(_CifBlk.read(StringIO(''.join(lines[blks_st[-1]:]))))
+        return cls(*blks)
+
+class _CifBlk:
+    """Object to handle CIF data block
+
+    Each block is a dict, containing three keys:
+        name: a string, charactering the data block
+        items: a dict, the simple key-value pair in CIF
+        loops: a list, each member is a dict containing "keys" (a list) and "values" (list of lists)
+            the member of values list is one entry containing data corresponding to "keys".
+    """
+    def __init__(self, name: str, items: dict, loops: List[dict]):
+        self._name = name
+        self._items = items
+        self._loops = loops
+
+    @property
+    def name(self):
+        """the name of the data block"""
+        return self._name
+
+    def get_item(self, key):
+        """get the value of an item.
+
+        Returns:
+            str, if the key is found in item
+            a tuple of str, if the key is found in the loops
+        """
+        v = self._items.get(key, None)
+        if v is None:
+            for loop in self._loops:
+                if key in loop["keys"]:
+                    i = loop["keys"].index(key)
+                    return tuple(e[i] for e in loop["values"])
+        else:
+            return v
+        raise KeyError(f"key {key} is not found in items or loops")
+
+    def get_loop(self, key):
+        """get the loop structure where the key lives
+
+        Returns:
+            an iterator
+        """
+        for loop in self._loops:
+            if key in loop["keys"]:
+                for entry in len(loop["values"]):
+                    d = dict(zip(loop["keys"], entry))
+                    yield d
+
+    @property
+    def items(self):
+        """items"""
+        return self._items
+
+    @property
+    def loops(self):
+        """loop data"""
+        return self._loops
+
+    # pylint: disable=R0912,R0914,R0915
+    @classmethod
+    def read(cls, pcifblk):
+        """read the CIF block"""
+        with open_textio(pcifblk) as h:
+            # strip and filter empty lines
+            lines = [l.strip() for l in h.readlines() if l and not l.startswith("#")]
+
+        def _handle_multiline_value(st):
+            """st: the line index containing the starting semicolon"""
+            _l = lines[st]
+            # if the first column is a keyword ("_"), remove it since we only need value
+            if _l.startswith("_"):
+                _, _l = _l.split(maxsplit=1)
+            assert _l.startswith(";")
+            _l = "\"" + _l.lstrip(";")
+            if len(_l) > 1:
+                _l = _l + " "
+            _lines = [_l,]
+            ed = st + 1
+            while ed < len(lines):
+                _lines.append(lines[ed] + " ")
+                if lines[ed].startswith(";"):
+                    break
+                ed += 1
+            _lines[-1] = "\"" + _lines[-1].lstrip(";")
+            return ed, "".join(_lines)
+
+        name = lines[0].lstrip('data_')
+        items = {}
+        loops = []
+        i = 1
+
+        while i < len(lines):
+            l = lines[i]
+            _logger.debug("handling line %d: %s", i, l.rstrip())
+            # items
+            if l.startswith("_"):
+                try:
+                    k, v = l.split(maxsplit=1)
+                    if v.startswith(";"):
+                        i, v = _handle_multiline_value(i)
+                except ValueError:
+                    k = l
+                    i += 1
+                    if v.startswith(";"):
+                        i, v = _handle_multiline_value(i+1)
+                    else:
+                        v = l
+                items[k] = v
+                _logger.info("cif item %s = %s", k, v)
+                i += 1
+            # items defined in a loop
+            elif l.startswith("loop_"):
+                loop = {"keys": [], "values": []}
+                i += 1
+                # read in keys
+                while True:
+                    try:
+                        l = lines[i]
+                    except IndexError:
+                        break
+                    if l.startswith("_"):
+                        loop["keys"].append(l)
+                        _logger.info("loop key %s", l)
+                        i += 1
+                    else:
+                        break
+                # read in data
+                nkeys = len(loop["keys"])
+                datastr = ""
+                in_multiline = False
+                while not (l.startswith("_") or l.startswith("data_") or l.startswith("loop_")):
+                    if l.startswith(";"):
+                        l = " \"" + l[1:]
+                        # exiting multi-line environment
+                        if in_multiline:
+                            l = l[1:]
+                        in_multiline = not in_multiline
+                    else:
+                        l = " " + l
+                    datastr += l
+                    i += 1
+                    try:
+                        l = lines[i]
+                    except IndexError:
+                        break
+                _logger.debug("handling loop data string: %s", datastr)
+                m = [g.group().strip("\'\"")
+                     for g in re.finditer(r'([\'\"])?(?(1).*?\1|\S+)', datastr)]
+                nentries = len(m) // nkeys
+                for ie in range(nentries):
+                    loop["values"].append(m[nkeys*ie:nkeys*(ie+1)])
+                loops.append(loop)
+                _logger.info("loaded %d loop data, in %d entries", len(m), nentries)
+        return cls(name, items, loops)
 
 class Cif:
     """Class to read CIF files and initialize atomic data by PyCIFRW
@@ -80,6 +260,7 @@ class Cif:
 
     @property
     def latt(self):
+        """the lattice vector of the crystal cell"""
         return self.get_lattice_vectors()
 
     def __init_inequiv(self):
@@ -129,7 +310,7 @@ class Cif:
     def get_chemical_name(self):
         """Return the chemical names stored in the head of a CIF file
 
-        Returns
+        Returns:
             3 str, systematic name, mineral name and structure type
         """
         sys = None
@@ -155,7 +336,7 @@ class Cif:
     def get_lattice_vectors(self):
         """initialize the lattice vectors from cif
 
-        Returns
+        Returns:
             list, shape (3,3)
         """
         if self._latt is None:
